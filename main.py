fdfabe33ca20aeca74008af553627c781de003a6
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-04 13:00:44 +0400

    04.09.25 - Игра с угадыванием числа. Повторение цикла for.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-N = 3
-for i in range(N):
-    for j in range(N):
-        print(i, j)
-print('')
-N = 3
-for i in range(N):
-    for j in range(i, N):
-        print(i, j)
+import random
+r = random.randint(0,100)
+for i in range(1,100):
+    a = int(input("Введите число: "))
+    if a > r:
+        print("Введенное число слишком большое")
+        continue
+    elif a < r:
+        print("Введенное число слишком маленькое")
+        continue
+    else:
+        print("Победа!!!")
+        break
