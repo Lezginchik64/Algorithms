15f9bec1ee0fbc1056ee452af0f57a63d312c955
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-05 03:15:53 +0400

    04.09.25 - Задача «Числа Фибоначчи» + Доп варианты решения.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+#Вариант 1
 a = int(input())
 f1 = 0
 f2 = 1
 for _ in range(a):
-    temp = f2  # запомним старый f2
-    f2 = f1 + f2  # новый f2 = сумма
-    f1 = temp  # новый f1 = старый f2
+    oldf2 = f2
+    f2 = f1 + oldf2
+    f1 = oldf2
+print(f1)
+
+#Вариант 2
+for _ in range(a):
+    f1, f2 = f2, f1 + f2
 print(f1)
