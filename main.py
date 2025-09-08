a5d47188f71d1e9ec7326918ea8cc0243102f0e6
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-08 23:59:34 +0400

    05.09.25 - Задача «Следующее четное».

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-n = int(input())
-a = (n // 2 + 1) * 2 # формула, которая всегда дает следующее четное число после n.
-print(a)
-
+h1, m1, s1 = int(input()), int(input()), int(input())
+h2, m2, s2 = int(input()), int(input()), int(input())
+total1 = h1 * 3600 + m1 * 60 + s1
+total2 = h2 * 3600 + m2 * 60 + s2
+print(total2 - total1)
 
 
