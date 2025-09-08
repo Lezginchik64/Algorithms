a6a466cc6c08f991f28ee167afd01935b5a0c202
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-09 00:01:32 +0400

    08.09.25 - Задача «Разность времен».

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ h1, m1, s1 = int(input()), int(input()), int(input())
 h2, m2, s2 = int(input()), int(input()), int(input())
 total1 = h1 * 3600 + m1 * 60 + s1
 total2 = h2 * 3600 + m2 * 60 + s2
-print(total2 - total1)
+print(total2 - total1) #
 
 
