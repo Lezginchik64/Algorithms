7dcd00f659a2c5b029efd021bd98a6d4acd9a682
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-12 00:57:36 +0400

    12.09.25 - Задача «Ход короля».

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ else:
 
 # Вариант 2
 y1, x1, y2, x2 = int(input()), int(input()), int(input()), int(input())
-if abs(x1 - x2 ) <= 1 and abs(x1 - x2 ) <= 1:
+if abs(x1 - x2 ) <= 1 and abs(x1 - x2 ) <= 1:       # abs - это модуль
     print("YES")
 else:
     print("NO")
