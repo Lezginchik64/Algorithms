2fcffbea3f682ede7e52bbf87b83c5500be5d54b
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-12 00:36:09 +0400

    12.09.25 - Задача «Ход ладьи».

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-a, b, c, d = int(input()), int(input()), int(input()), int(input())
-if (a + b) % 2 == (c + d) % 2:
+y1, x1, y2, x2 = int(input()), int(input()), int(input()), int(input())
+if y1 == y2 or x1 == x2:
     print("YES")
 else:
     print("NO")
