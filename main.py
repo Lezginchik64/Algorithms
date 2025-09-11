b251c3a959bb8fc28dfc9210b68df804075737f6
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-12 00:56:59 +0400

    12.09.25 - Задача «Ход короля».

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+# Вариант 1
 y1, x1, y2, x2 = int(input()), int(input()), int(input()), int(input())
-if y1 == y2 or x1 == x2:
+if (y1 == y2 + 1 or y1 == y2 - 1) or (x1 == x2 + 1 or x1 == x2 - 1):
     print("YES")
 else:
     print("NO")
 
+# Вариант 2
+y1, x1, y2, x2 = int(input()), int(input()), int(input()), int(input())
+if abs(x1 - x2 ) <= 1 and abs(x1 - x2 ) <= 1:
+    print("YES")
+else:
+    print("NO")
