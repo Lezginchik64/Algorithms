aa30bf3287a5edd62ebce2afa8a41d7440f3ee43
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-14 02:23:40 +0400

    13.09.25 - Задача «Ход слона».

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-# Вариант 1
 y1, x1, y2, x2 = int(input()), int(input()), int(input()), int(input())
-if (y1 == y2 + 1 or y1 == y2 - 1) or (x1 == x2 + 1 or x1 == x2 - 1):
-    print("YES")
+if abs(x1 - x2) == abs(y1 - y2):
+    print('YES')
 else:
-    print("NO")
-
-# Вариант 2
-y1, x1, y2, x2 = int(input()), int(input()), int(input()), int(input())
-if abs(x1 - x2 ) <= 1 and abs(x1 - x2 ) <= 1:       # abs - это модуль
-    print("YES")
-else:
-    print("NO")
+    print('NO')
