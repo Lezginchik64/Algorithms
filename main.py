e86ba40734ae39f9293cbdc12de339bc0be06cb3
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-14 03:04:36 +0400

    13.09.25 - Задача «Шоколадка».

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-y1, x1, y2, x2 = int(input()), int(input()), int(input()), int(input())
-dx, dy = abs(x1 - x2), abs(y1 - y2)
-if dx == 2 and dy == 1 or dx == 1 and dy == 2:
+n, m, k = int(input()), int(input()), int(input())
+s = n * m
+if (k % n == 0 or k % m == 0) and k < s:
     print("YES")
 else:
     print("NO")
