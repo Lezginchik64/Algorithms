dc57c6547e6ce071660089fdcd554fe0fd013f43
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-14 02:48:13 +0400

    13.09.25 - Задача «Ход коня».

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 y1, x1, y2, x2 = int(input()), int(input()), int(input()), int(input())
-if abs(x1 - x2) == abs(y1 - y2):
-    print('YES')
+dx, dy = abs(x1 - x2), abs(y1 - y2)
+if dx == 2 and dy == 1 or dx == 1 and dy == 2:
+    print("YES")
 else:
-    print('NO')
+    print("NO")
