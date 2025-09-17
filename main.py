b3ac1570be307c05e57dc4e5ef2da470739bbd64
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-17 14:52:52 +0400

    17.09.25 - код для бота Quiz_bot.

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-d = int(input())
-v1 = int(input())
-v2 = int(input())
-t = int(input())
-coord1 = v1 * t % d                   # Координаты 1 бегуна. % d - так как дорожка круговая.
-coord2 = v2 * t % d                   # Координаты 2 бегуна.
-dist = abs(coord1 - coord2)           # Разница между координатами. abs - модуль.
-print(min(dist , d - dist))
-                                      # На круге всегда есть два пути между бегунами:
-                                        # 1. Вперёд по часовой стрелке (длина dist),
-                                        # 2. Назад против часовой стрелки (длина d - dist).
-                                      # Берём минимальное из двух (min) — это и есть минимальное расстояние между бегунами.
+import asyncio
+import logging
+from aiogram.filters.command import Command
+from aiogram import Bot, Dispatcher, types
+from db.database import create_table
+from handlers import start, quiz, results
+
+
+API_TOKEN = ""
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+# Регистрация хэндлеров
+start.register_handlers(dp)
+quiz.register_handlers(dp)
+results.register_handlers(dp)
+
+
+@dp.message(Command("myid"))
+async def cmd_myid(message: types.Message):
+    await message.answer(f"Ваш ID: {message.from_user.id}")
+
+async def main():
+    await create_table()
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
