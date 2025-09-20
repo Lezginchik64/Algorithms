8b8b1bfd9a3e5666416f92d965e349501c193745
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-21 01:33:19 +0400

    20.09.25 - Задача «Список квадратов».

--- main.py
+++ main.py
@@ -1,32 +1,5 @@
-import asyncio
-import logging
-from aiogram.filters.command import Command
-from aiogram import Bot, Dispatcher, types
-from db.database import create_table
-from handlers import start, quiz, results
-
-
-API_TOKEN = ""
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
-
-# Регистрация хэндлеров
-start.register_handlers(dp)
-quiz.register_handlers(dp)
-results.register_handlers(dp)
-
-
-@dp.message(Command("myid"))
-async def cmd_myid(message: types.Message):
-    await message.answer(f"Ваш ID: {message.from_user.id}")
-
-async def main():
-    await create_table()
-    await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
+N = int(input())
+i = 1 
+while i * i <= N:
+    print(i * i, end = ' ')
+    i += 1
