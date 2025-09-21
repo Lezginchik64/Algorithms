0649130dc7853e01675e855f52bf7e36056bb14b
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-21 13:22:24 +0400

    21.09.25 - Задача «Степень двойки»

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+# Вариант 1
 n = int(input())
-i = 2
-while n % i != 0:
+i = 1                   # i - степень двойки
+while 2 ** i <= n:
     i += 1
-print(i)
+print(i - 1, 2 ** (i - 1))
+
+# Вариант 2
+n = int(input())
+deg, two = 0, 1             # deg - степень двойки, two - результат двойки в степени.
+while two * 2 <= n:         # two равен 1, потому что 2 в степени 0 = 1
+    deg += 1
+    two *= 2
+print(deg, two)
 
