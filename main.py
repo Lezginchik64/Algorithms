f872dfe86984fc17fb30c8c7e272ff7d35ca1705
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-23 10:50:10 +0400

    23.09.25 - Задача «Утренняя пробежка»

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-# Вариант 1
-n = int(input())
-i = 1                   # i - степень двойки
-while 2 ** i <= n:
-    i += 1
-print(i - 1, 2 ** (i - 1))
-
-# Вариант 2
-n = int(input())
-deg, two = 0, 1             # deg - степень двойки, two - результат двойки в степени.
-while two * 2 <= n:         # two равен 1, потому что 2 в степени 0 = 1
-    deg += 1
-    two *= 2
-print(deg, two)
-
+x = int(input())
+y = int(input())
+c = 1
+while x < y:
+    d = x * 0.1
+    x += d
+    c += 1
+    print(x)
+print(c)
