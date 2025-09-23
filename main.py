91eda53c3d8c7037b5a2eab8ad8fdb0083b773cd
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-24 00:03:34 +0400

    Задача «Сумма последовательности»

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# 1 Вариант (не совсем корректный, но рабочий)
-count = 0
-while True:
-    n = int(input())
-    if n == 0:
-        break
-    count += 1
-print(count)
-
-# 2 Вариант
-count = 0
+sum = 0
 a = int(input())
 while a != 0:
-    count += 1
+    sum += a
     a = int(input())
-print(count)
+print(sum)
