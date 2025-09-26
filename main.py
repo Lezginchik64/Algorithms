1f277dcdc9d9e0d387b343d3b39a118a01a2bf33
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-27 00:47:39 +0400

    26.09. Задача «Количество элементов, равных максимуму»

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-prev = int(input())
-cur = int(input())
-count = 0
+# Вариант 1
+a = int(input())
+m = countm = 0
+while a != 0:
+    if a > m:
+        m, countm = a, 1   # нашли новый максимум → обновили m и сбросили счётчик
+    elif a == m:
+        countm += 1        # нашли ещё одно вхождение максимума
+    a = int(input())
+print(countm)
 
-while cur != 0:
-    if cur > prev:
-        count += 1
-    prev = cur
-    cur = int(input())
-print(count)
+# Вариант 2
+m = 0
+c = 1
+a = int(input())
+while a != 0:
+    if a > m:
+        m = a
+    a = int(input())
+    if a == m:
+        c += 1
+print(c)
+
+
