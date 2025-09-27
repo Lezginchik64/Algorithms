ecb61b14520cb2ca3c6aa0695a3a966a85e1b913
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-09-27 14:57:20 +0400

    27.09. Задача «Номер числа Фибоначчи» с циклом while

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-# Вариант 1
 a = int(input())
-m = countm = 0
-while a != 0:
-    if a > m:
-        m, countm = a, 1   # нашли новый максимум → обновили m и сбросили счётчик
-    elif a == m:
-        countm += 1        # нашли ещё одно вхождение максимума
-    a = int(input())
-print(countm)
-
-# Вариант 2
-m = 0
-c = 1
-a = int(input())
-while a != 0:
-    if a > m:
-        m = a
-    a = int(input())
-    if a == m:
-        c += 1
-print(c)
-
-
+f1, f2 = 0, 1
+i = 1
+while f2 < a:                 # Цикл работает до числа Фибоначчи
+    f1, f2 = f2, f1 + f2
+    i += 1
+if a == f2:
+    print(i)
+else:
+    print(-1)
