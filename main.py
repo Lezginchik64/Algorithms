ac9eb9d12d3fd107e7aa941a9f61dc23be7cfd65
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-02 00:12:30 +0400

    01.10. Задача «Первое и последнее вхождения»

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-# Option 1
 s = input()
-ind = s.find(' ')
-word1 = s[:ind]
-word2 = s[ind + 1:]
-print(word2, word1)
 
-# Option 2
-s = input()
-if s.count(" ") >= 1:
-    print(s[s.find(" ") + 1:] + " " + s[0:s.find(" ")])
+if s.count("f") == 1:
+    print(s.find("f"))
+elif s.count("f") >= 2:
+    print(s.find("f"), s.rfind("f"))
