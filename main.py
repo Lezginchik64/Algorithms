aa5f9ef85224b91b2b09ddf934cb07e7e3ced3e8
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-01 23:27:21 +0400

    01.10. Задача «Две половинки»

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-s = str(input())
-words = s.count(" ") + 1
-print(words)
+# Вариант 1
+s = input()
+ind = (len(s) + 1) // 2
+part1 = s[:ind]
+part2 = s[ind:]
+print(part2 + part1)
+
+# Вариант 2
+s = input()
+mid = len(s) // 2
+if len(s) % 2 != 0:
+    print(s[mid+1:] + s[:mid+1])
+else:
+    print(s[mid:] + s[:mid])
