bc3e38dabdef6eeb6e7eab69b8b1f1fee5372d04
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-02 00:18:52 +0400

    01.10. Задача «Второе вхождение»

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+# Option 1
 s = input()
+if s.count('f') == 1:
+    print(-1)
+elif s.count('f') < 1:
+    print(-2)
+else:
+    print(s.find('f', s.find('f') + 1))
 
+# Option 2
+s = input()
 if s.count("f") == 1:
-    print(s.find("f"))
-elif s.count("f") >= 2:
-    print(s.find("f"), s.rfind("f"))
+    print(-1)
+elif s.count('f') < 1:
+    print(-2)
+else:
+    print(s.rfind("f"))
