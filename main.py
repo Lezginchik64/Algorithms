98585a02bdcabb6fbc156cadc9dcef91afee1e65
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-04 00:29:16 +0400

    03.10. Задача «Удаление фрагмента»

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-# Option 1
 s = input()
-if s.count('f') == 1:
-    print(-1)
-elif s.count('f') < 1:
-    print(-2)
-else:
-    print(s.find('f', s.find('f') + 1))
+if s.find("h") > 1:
+    print(s[:s.find("h")] + s[s.rfind("h") + 1:])
 
-# Option 2
-s = input()
-if s.count("f") == 1:
-    print(-1)
-elif s.count('f') < 1:
-    print(-2)
-else:
-    print(s.rfind("f"))
+
