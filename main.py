f555d19600b0d2f778762f22e08252c0bd1d4160
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-06 01:18:20 +0400

    05.10. Задача «Замена внутри фрагмента» Вырез части + Замена

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 s = input()
-print(s.replace("1", "one"))
+first, second = s.find("h"), s.rfind("h")
+part1 = s[:first + 1]
+part2 = s[second:]
+middle = s[first+1:second].replace("h", "H")
+print(part1 + middle + part2)
+
 
