b298ee4c3393d4489ace620ed04d82b0c088e866
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-07 02:17:18 +0400

    06.10. Генераторы

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-# Вариант 1
-s = input()
-n = int(input())
-english = "abcdefghijklmnopqrstuvwxyz"
-count = ""
-for i in s:
-    index = english.find(i)                     # Ищем индекс введенной буквы
-    new_index = (index + n) % len(english)      # прибавляем к индексу n, делим на длину алфавита, чтобы проходить по нему заново
-    res = english[new_index]                    # находим новую букву с новым индексом
-    count += res                                # добавляем в счетчик
-print(count)
+# Генераторы - выражения, позволяющие заполнить список некоторой формулой.
 
-# Вариант 2
-from string import ascii_lowercase              # импорт строки 'abcdefghijklmnopqrstuvwxyz'
-s = input()
-n = int(input()) % 26                           # получаем сдвиг и нормализуем его в диапазон 0-25
-res = ''
-for x in s:
-    res += ascii_lowercase[(ascii_lowercase.find(x) + n) % 26]          # ascii_lowercase.find(x) - находим позицию символа в алфавите
-print(res)                                                              # + n - сдвигаем позицию на значение n
-                                                                        # % 26 - обеспечиваем циклический сдвиг (если вышли за 'z', возвращаемся к 'a')
-                                                                        # ascii_lowercase[] - получаем новый символ из алфавита по вычисленной позиции
+# Пример 1
+n = 5
+a = [i ** 2 for i in range(n)]
+
+# Пример 2
+from random import randrange
+n = 10
+a = [randrange(1, 10) for i in range(n)]
+
+# Пример 3
+a = [input() for i in range(int(input()))]
+
+# Пример 4
+a = [i for i in range(5) if i % 2 == 0]
+print(*a) # 0 2 4
