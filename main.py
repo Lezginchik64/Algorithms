3b56f3617f3d3687d46f848ea1f876e876d948d0
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-07 23:13:15 +0400

    IP search

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-a = input().split()
-print(*[a[i] for i in range(len(a)) if int(a[i]) % 2 == 0])
+import requests
+
+def get_location_from_ip(ip_address):
+    try:
+        url = f"https://ipinfo.io/{ip_address}"
+        response = requests.get(url)
+        data = response.json()
+
+        params = ['country', 'city', 'region', 'loc']
+        location = []
+        for i in params:
+            location.append(data.get(i, None))
+        return location
+    except Exception as e:
+        return f"Ошибка при получении местоположения: {e}"
+
+ip_address = "213.89.188.212"
+location = get_location_from_ip(ip_address)
+
+print(f"Местоположение для IP-адреса {ip_address}: {location}")
