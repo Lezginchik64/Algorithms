01b810a882884df46f773c75a3a1a459cdaab71f
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-07 16:38:57 +0400

    07.10. Задача «Четные элементы»

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-#Option 1
-print(*input().split()[::2])
-
-#Option 2
 a = input().split()
-print(*[a[i] for i in range(len(a)) if i % 2 == 0])
+print(*[a[i] for i in range(len(a)) if int(a[i]) % 2 == 0])
