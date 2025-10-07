047d7ed41ace6861527034b51ccbd7bb92e3f6a8
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-08 01:24:22 +0400

    26.09. Задача «Второй максимум». Собес

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-import requests
-
-def get_location_from_ip(ip_address):
-    try:
-        url = f"https://ipinfo.io/{ip_address}"
-        response = requests.get(url)
-        data = response.json()
-
-        params = ['country', 'city', 'region', 'loc']
-        location = []
-        for i in params:
-            location.append(data.get(i, None))
-        return location
-    except Exception as e:
-        return f"Ошибка при получении местоположения: {e}"
-
-ip_address = "213.89.188.212"
-location = get_location_from_ip(ip_address)
-
-print(f"Местоположение для IP-адреса {ip_address}: {location}")
+m1 = m2 = 0
+a = int(input())
+while a != 0:
+    if a > m1:
+        m1, m2 = a, m1
+    elif a > m2:
+        m2 = a
+    a = int(input())
+print(m2)
