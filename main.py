e4a4b3051e89524a37cf9bf3ab57dc49eaddf502
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-08 13:07:36 +0400

    08.10. Задача «Больше предыдущего» Исправление.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#Option 1
+#Option 1  -  выполнена неверно, так как элементы списка являются строками
 a = input().split()
 for i in range(1, len(a)):    # Начинаем от 1, чтобы не было такого a[0] > a[-1]
     if a[i] > a[i - 1]:
         print(a[i], end=' ')
 
 #Option 2
-a = [int(x) for x in input().split()]
-for i in range(1, len(a)):
-    if a[i] > a[i - 1]:
-        print(a[i], end = ' ')
+a1 = [int(x) for x in input().split()]  #Преобразуем элементы списка в числа
+for i in range(1, len(a1)):
+    if a1[i] > a1[i - 1]:
+        print(a1[i], end = ' ')
