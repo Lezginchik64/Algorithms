b575767cadee2f7f5d6410ad73e6e6283e550b6c
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-08 20:45:04 +0400

    08.10. Задача «Наибольший элемент»

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
 a = [int(x) for x in input().split()]
-count = 0
-for i in range(1, len(a) - 1):               # Идем от второго до предпоследнего, чтобы сработал a[i + 1]
-    if a[i - 1] < a[i] and a[i + 1] < a[i]:
-        count += 1
-print(count)
+print(max(a), a.index(max(a)))
