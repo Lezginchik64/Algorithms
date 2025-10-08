ed434b129b7436de94deee1f739301fd28773ddf
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-08 20:09:45 +0400

    08.10. Задача «Соседи одного знака»

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-#Option 1  -  выполнена неверно, так как элементы списка являются строками
-a = input().split()
-for i in range(1, len(a)):    # Начинаем от 1, чтобы не было такого a[0] > a[-1]
-    if a[i] > a[i - 1]:
-        print(a[i], end=' ')
-
-#Option 2
-a1 = [int(x) for x in input().split()]  #Преобразуем элементы списка в числа
-for i in range(1, len(a1)):
-    if a1[i] > a1[i - 1]:
-        print(a1[i], end = ' ')
+a = [int(x) for x in input().split()]
+for i in range(1, len(a)):
+    if a[i] * a[i - 1] > 0:
+        print(a[i-1], a[i], end = ' ')
+        break
