3d333feae3865575e2a7578a9a25060f9c269828
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-08 22:20:51 +0400

    08.10. Задача «Шеренга»

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+#Option 1
 a = [int(x) for x in input().split()]
-print(max(a), a.index(max(a)))
+petya = int(input())
+for i in range(len(a)):
+    if petya > a[i]:
+        print(i + 1)
+        break
+else:
+    print(len(a) + 1)
+
+#Option 2
+a = [int(x) for x in input().split()]
+X = int(input())
+pos = 0
+while pos < len(a) and X <= a[pos]:
+    pos += 1
+print(pos + 1)
