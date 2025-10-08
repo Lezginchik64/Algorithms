d3d2f56c29a25f4f1facd85e255553a0902ddc0f
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-08 20:20:52 +0400

    08.10. Задача «Больше своих соседей»

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 a = [int(x) for x in input().split()]
-for i in range(1, len(a)):
-    if a[i] * a[i - 1] > 0:
-        print(a[i-1], a[i], end = ' ')
-        break
+count = 0
+for i in range(1, len(a) - 1):               # Идем от второго до предпоследнего, чтобы сработал a[i + 1]
+    if a[i - 1] < a[i] and a[i + 1] < a[i]:
+        count += 1
+print(count)
