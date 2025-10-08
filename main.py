318373309e6d8b1fa968dcf532e3d7d8c7eb9f3d
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-09 01:15:35 +0400

    08.10. Задача «Количество различных элементов»

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-#Option 1
 a = [int(x) for x in input().split()]
-petya = int(input())
-for i in range(len(a)):
-    if petya > a[i]:
-        print(i + 1)
-        break
-else:
-    print(len(a) + 1)
-
-#Option 2
-a = [int(x) for x in input().split()]
-X = int(input())
-pos = 0
-while pos < len(a) and X <= a[pos]:
-    pos += 1
-print(pos + 1)
+count = 1
+for i in range(1, len(a)):
+    if a[i] != a[i - 1]:
+        count += 1
+print(count)
