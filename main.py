c8f99dfb91cdb14220ae21dbc7e421d60d485b7c
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-10 01:17:16 +0400

    09.10. Задача «Удалить элемент»

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 a = [int(x) for x in input().split()]
-idx_min = a.index(min(a))           # Индекс макс или мин используем тогда,когда нужно менять элементы местами
-idx_max = a.index(max(a))
-a[idx_min], a[idx_max] = a[idx_max], a[idx_min]
+k = int(input())
+a.pop(k)
 print(*a)
