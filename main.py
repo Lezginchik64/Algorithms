1cac00b767423787848f5b2a3a91df92c088214a
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-11 02:20:37 +0400

    10.10. Задача «Вставить элемент»

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+#Option 1
 a = [int(x) for x in input().split()]
-k = int(input())
-a.pop(k)
+k, c = [int(x) for x in input().split()]
+a = a[:k] + [c] + a[k:]
+print(*a)
+
+#Option 2
+a = [int(x) for x in input().split()]
+k, C = [int(x) for x in input().split()]
+
+a.append(0)                 # Увеличивает список, добавляя 0 в конец
+for i in range(len(a) - 1, k, -1):   # Идем в обратном порядке
+    a[i] = a[i - 1]                    # Сдвигаем элементы
+a[k] = C
+
 print(*a)
