b229fa9650df5d1611d6baf561397f92f5d43db6
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-13 01:29:58 +0400

    12.10. Задача «Циклический сдвиг вправо»

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-#Option 1
 a = [int(x) for x in input().split()]
-k, c = [int(x) for x in input().split()]
-a = a[:k] + [c] + a[k:]
-print(*a)
-
-#Option 2
-a = [int(x) for x in input().split()]
-k, C = [int(x) for x in input().split()]
-
-a.append(0)                 # Увеличивает список, добавляя 0 в конец
-for i in range(len(a) - 1, k, -1):   # Идем в обратном порядке
-    a[i] = a[i - 1]                    # Сдвигаем элементы
-a[k] = C
-
+last = a[-1]
+for i in range(len(a) - 1, 0, -1):
+    a[i] = a[i - 1]
+a[0] = last
 print(*a)
