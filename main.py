1544b6fb1c8708d51a309135135f148181f302cb
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-13 13:46:16 +0400

    13.10. Задача «Голосование»

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-def sum_of_digits(n):
-    s = 0
-    while n > 0:
-        s += n % 10
-        n //= 10
-    return s
+# Option 1
+def election(x, y, z):
+    if x + y + z >= 2:
+        return 1
+    else:
+        return 0
 
-n = int(input())
-print(sum_of_digits(n))
+x, y, z = [int(x) for x in input().split()]
+print(election(x, y, z))
+
+# Option 2
+def election(x, y, z):
+    votes = [x, y, z]
+    if votes.count(1) > votes.count(0):
+        return 1
+    else:
+        return 0
+x, y, z = [int(x) for x in input().split()]
+print(election(x, y, z))
