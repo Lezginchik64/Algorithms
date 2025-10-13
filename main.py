1694a6e83af826544efe058fe91fb2e08dedad61
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-13 12:47:46 +0400

    13.10. "Функции"

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-a = [int(x) for x in input().split()]
-count = 0
-for i in range(len(a)):
-    for j in range(i + 1, len(a)):
-        if a[i] == a[j]:
-            count += 1
-print(count)
+# Функции
+# Пример 1
+def factorial(n):       # n - параметр
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res         # завершает работу функции и возвращает значение переменной res
+
+print(factorial(3))
+print(factorial(5))
+
+# Пример 2
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+print(max(3, 5))
+print(max(5, 3))
+print(max(int(input()), int(input())))
+
+# Пример 3
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+def max3(a, b, c):
+    return max(max(a, b), c)
+
+print(max3(3, 5, 4))
