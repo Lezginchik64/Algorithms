01569b399f73870b5997de605ace33d35849c08c
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-13 13:35:36 +0400

    13.10. Задача «Сумма цифр»

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def is_palindrome(s):
-    return s == s[::-1]
+def sum_of_digits(n):
+    s = 0
+    while n > 0:
+        s += n % 10
+        n //= 10
+    return s
 
-print(is_palindrome(input()))
+n = int(input())
+print(sum_of_digits(n))
