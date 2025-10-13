384c364867ac96e2e4b09c507d608fbb3eeae4d2
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-13 13:12:17 +0400

    13.10. Задача «Палиндром»

--- main.py
+++ main.py
@@ -1,33 +1,4 @@
-# Функции
-# Пример 1
-def factorial(n):       # n - параметр
-    res = 1
-    for i in range(1, n + 1):
-        res *= i
-    return res         # завершает работу функции и возвращает значение переменной res
+def is_palindrome(s):
+    return s == s[::-1]
 
-print(factorial(3))
-print(factorial(5))
-
-# Пример 2
-def max(a, b):
-    if a > b:
-        return a
-    else:
-        return b
-
-print(max(3, 5))
-print(max(5, 3))
-print(max(int(input()), int(input())))
-
-# Пример 3
-def max(a, b):
-    if a > b:
-        return a
-    else:
-        return b
-
-def max3(a, b, c):
-    return max(max(a, b), c)
-
-print(max3(3, 5, 4))
+print(is_palindrome(input()))
