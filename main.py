d505ce1f786f98b4d867084adaaca82d73ff4bb0
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-14 01:55:28 +0400

    13.10. Задача «Определение чётности»

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-# Option 1
-def election(x, y, z):
-    if x + y + z >= 2:
-        return 1
+def even_or_odd(n):
+    if n % 2 == 0:
+        return "even"
     else:
-        return 0
+        return "odd"
 
-x, y, z = [int(x) for x in input().split()]
-print(election(x, y, z))
-
-# Option 2
-def election(x, y, z):
-    votes = [x, y, z]
-    if votes.count(1) > votes.count(0):
-        return 1
-    else:
-        return 0
-x, y, z = [int(x) for x in input().split()]
-print(election(x, y, z))
+print(even_or_odd(int(input())))
