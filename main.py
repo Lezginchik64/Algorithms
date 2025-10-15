243694699de2ec89169dae2a7b1f9bf641a4fcc3
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-15 13:27:34 +0400

    15.10. Задача «Факториал»

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from math import sqrt
-def distance(x1, y1, x2, y2):
-    dx = x2 - x1
-    dy = y2 - y1
-    return sqrt(dx**2 + dy**2)
+def factorial(n):
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res
 
-x1, y1 = float(input()), float(input())
-x2, y2 = float(input()), float(input())
-print(distance(x1, y1, x2, y2))
+
+print(factorial(int(input())))
