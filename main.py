b8649c95df4bf58a2663be9e7f11c8ed527667da
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-15 13:22:35 +0400

    15.10. Задача «Длина отрезка»

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def greet(name):
-    print(f'Hello, {name}!')
-N = int(input())
-names = [input() for _ in range(N)]
-for name in names:
-    greet(name)
+from math import sqrt
+def distance(x1, y1, x2, y2):
+    dx = x2 - x1
+    dy = y2 - y1
+    return sqrt(dx**2 + dy**2)
+
+x1, y1 = float(input()), float(input())
+x2, y2 = float(input()), float(input())
+print(distance(x1, y1, x2, y2))
