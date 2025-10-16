f532597c5c8961e58e12319d05cea0958ae70f35
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-16 19:52:54 +0400

    16.10. Задача «Сумма арифметической прогрессии»

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-def isprime(n):
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
+# Вариант 1
+def arithmetic_progression(a, d, n):
+    an = a + (n - 1) * d
+    sn = n * (a + an) // 2
+    return sn
 
+a, d, n = [int(x) for x in input().split()]
+print(arithmetic_progression(a, d, n))
 
-n = int(input())
-print(isprime(n))
+# Вариант 2
+def arithmetic_progression2(a, d, n):
+    return (2 * a + (n - 1) * d) * n // 2
+
+a, d, n = [int(x) for x in input().split()]
+print(arithmetic_progression2(a, d, n))
+
+# Вариант 3
+def arithmetic_progression3(a, d, n):
+    total = 0
+    for i in range(n):
+        total += a + i * d
+    return total
+a, d, n = [int(x) for x in input().split()]
+print(arithmetic_progression3(a, d, n))
