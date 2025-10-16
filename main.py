abce594ffff2f6d83c564ac352e6373630b9595c
Vadim Pirimov <vadim.pirimov.01@mail.ru>
2025-10-17 01:30:08 +0400

    16.10. Задача «Без двух букв»

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def median(nums):
-    nums = sorted(nums)
-    n = len(nums)
-    if n % 2 != 0:
-        return nums[n // 2]
-    else:
-        return int(round((nums[n // 2] + nums[n // 2 - 1]) / 2, 0))
-# длина списка делится пополам, затем этот элемент складывается с предыдущим элементом и делится на 2, в конце округляется
-nums = [int(x) for x in input().split()]
-print(median(nums))
+def check(s):
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1]:
+            return False
+    return True
+
+print(check(input()))
